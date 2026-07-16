515f19e11475e47e4354d654ce9e3a1972919dd4
Scho1990 <it.santosh68@gmail.com>
2026-07-16 19:31:27 +0530

    PR-4 Phase-2: Implemented each validator methods inside the ConfigurationValidator class

--- src/main/java/lifecycle/ConfigurationValidator.java
+++ src/main/java/lifecycle/ConfigurationValidator.java
@@ -1,15 +1,17 @@
 package lifecycle;
 
+import config.ConfigReader;
+import enums.BrowserType;
+import exceptions.ConfigurationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Arrays;
 import java.util.List;
 
 public final class ConfigurationValidator {
 
     private static final Logger logger = LogManager.getLogger(ConfigurationValidator.class);
-    private static final List<String> REQUIRED_PROPERTIES = List.of("erail.url",
+    private static final List<String> REQUIRED_PROPERTIES = List.of(
+            "erail.url",
             "orangehrm.url",
             "browser",
             "headless",
@@ -32,13 +34,38 @@ public final class ConfigurationValidator {
 
     private static void validateRequiredProperties(){
         logger.info("Validating required properties...");
+        for(String requiredProperty : REQUIRED_PROPERTIES){
+            String value  = ConfigReader.getProperty(requiredProperty);
+            if(value == null || value.isBlank()){
+                logger.info("Required property {} not found.",requiredProperty);
+                throw new ConfigurationException("Required property '%s' not found.".formatted(requiredProperty));
+            }
+        }
     }
 
     private static void validateBrowser(){
         logger.info("validating browser...");
+        String browser = ConfigReader.getProperty("browser");
+        try {
+            BrowserType.valueOf(browser.toUpperCase());
+        }
+        catch (IllegalArgumentException e){
+            logger.info("Browser {} not found.",browser);
+            throw new ConfigurationException("Browser '%s' not found.".formatted(browser));
+        }
     }
 
     private static void validateTimeouts(){
         logger.info("validating timeouts...");
+        validatePositiveNumber("implicit.wait");
+        validatePositiveNumber("explicit.wait");
+        validatePositiveNumber("page.load.timeout");
+    }
+
+    private static void validatePositiveNumber(String property) {
+        int value = ConfigReader.getIntProperty(property);
+        if (value <= 0) {
+            throw new ConfigurationException("'%s' must be greater than zero.".formatted(property));
+        }
     }
 }
