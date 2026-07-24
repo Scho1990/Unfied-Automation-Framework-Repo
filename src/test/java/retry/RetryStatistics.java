ac5c84b941372c1b24eb3e09941d5aebcf44ed9b
Scho1990 <it.santosh68@gmail.com>
2026-07-24 22:50:28 +0530

    Phase 1: RetryStatistics class implementaion done and now I have to call in RetryAnalyzer class in next commit

--- src/test/java/retry/RetryStatistics.java
+++ src/test/java/retry/RetryStatistics.java
@@ -1,5 +1,7 @@
 package retry;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,5 +13,54 @@ public final class RetryStatistics {
     private static final AtomicInteger passedAfterRetry  = new AtomicInteger();
     private static final AtomicInteger failedAfterRetry = new AtomicInteger();
 
-    ConcurrentHashMap<String, Integer> retryCountPerTest = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Integer> retryCountPerTest = new ConcurrentHashMap<>();
+
+    public static void incrementRetryAttempts() {
+        totalRetryAttempts.incrementAndGet();
+    }
+
+    public static void incrementRetriedTests() {
+        totalRetriedTests.incrementAndGet();
+    }
+
+    public static void incrementPassedAfterRetry() {
+        passedAfterRetry.incrementAndGet();
+    }
+
+    public static void incrementFailedAfterRetry() {
+        failedAfterRetry.incrementAndGet();
+    }
+
+    public static void recordRetry(String testName) {
+        retryCountPerTest.merge(testName, 1, Integer::sum);
+    }
+
+    public static int getTotalRetryAttempts() {
+        return totalRetryAttempts.get();
+    }
+
+    public static int getTotalRetriedTests() {
+        return totalRetriedTests.get();
+    }
+
+    public static int getPassedAfterRetry() {
+        return passedAfterRetry.get();
+    }
+
+    public static int getFailedAfterRetry() {
+        return failedAfterRetry.get();
+    }
+
+    public static Map<String, Integer> getRetryCountPerTest() {
+        return Collections.unmodifiableMap(retryCountPerTest);
+    }
+
+    public static void reset() {
+        totalRetryAttempts.set(0);
+        totalRetriedTests.set(0);
+        passedAfterRetry.set(0);
+        failedAfterRetry.set(0);
+        retryCountPerTest.clear();
+    }
+
 }
